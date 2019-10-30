e17af8f6a44b30d48b28518f5d0b1da98f6e3926
Tatiana Gripasova <botnar.tatiana@gmail.com>
2019-10-30 15:04:44 -0400

    Add filters screen styles

--- screens/FilterResturants.js
+++ screens/FilterResturants.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet }  from "react-native";
+import { View, StyleSheet, Picker } from "react-native";
 import Modal from "react-native-modal";
 import RadioForm from "react-native-simple-radio-button";
 import Header from "../components/Header";
@@ -17,10 +17,6 @@ const FilterRestaurants = props => {
         setPrice(price);
     };
 
-    const radio_rests = radioRests.map((food, i) => (
-        { label: food, value: i }
-    ));
-    
     const radio_prices = radioPrices.map((price, i) => (
         { label: price, value: i }
     ));
@@ -34,35 +30,54 @@ const FilterRestaurants = props => {
     };
 
     return (
-        <Modal style={styles.modal} isVisible={props.visible} coverScreen={true} backdropColor={"white"} backdropOpacity={1} >
-        <Header title="Choose a Cuisine" />
-        <View>
-            <RadioForm
-            radio_props={radio_rests}
-            initial={cuisine}
-            animation={true}
-            onPress={choosingCuisine}
-            labelStyle={{ fontSize: 20, color: "black", margin: 10}}
-            />
-        </View>
-        <Header title="Pricing" />
-        <View style={styles.formRests}>
-        <RadioForm
-            radio_props={radio_prices}
-            initial={price}
-            formHorizontal={true}
-            animation={true}
-            onPress={choosingPricing}
-            labelStyle={{ fontSize: 20, color: "black", margin: 5 }}
-            />
-        </View>  
-        <ImageButton style={styles.submitButton}  
-            source={require("../images/submit.png")} onPress={applyFilters} />
-        <ImageButton style={styles.cancelButton} 
-            imageStyle={styles.cancelButtonImage}
-            source={require("../images/cancel.png")}
-            onPress={props.hideFilterModal}
-        />
+        <Modal 
+            style={styles.modal} 
+            isVisible={props.visible} 
+            coverScreen={true} 
+            backdropColor={"#FFFFFF"}
+            backdropOpacity={1} 
+        >
+            <Header title="Choose a Cuisine" />
+            <View style={styles.wrapper}>
+                <View style={styles.picker}>
+                    <Picker
+                        itemStyle={{ fontSize: 22, fontFamily: "System" }}
+                        style={{height: 100, width: 200}}
+                        selectedValue={cuisine}
+                        onValueChange={choosingCuisine}
+                    >
+                    {radioRests.map((food, i) => 
+                        (<Picker.Item key={i} label={food} value={i} />))
+                    }
+                    </Picker>
+                </View>
+                <View style={styles.formRests}>
+                    <Header title="Pricing" />
+                    <RadioForm
+                        radio_props={radio_prices}
+                        initial={price}
+                        formHorizontal={true}
+                        animation={true}
+                        onPress={choosingPricing}
+                        buttonColor={"#000000"}
+                        selectedButtonColor={"#000000"}
+                        labelStyle={{ fontSize: 16, color: "#000000", margin: 5 }}
+                    />
+                </View>  
+                <View style={{ flex: 1}}>
+                    <ImageButton 
+                        source={require("../images/submit.png")} 
+                        onPress={applyFilters} 
+                    />
+                </View>
+                <View style={{ flex: 1 }}>
+                    <ImageButton
+                        imageStyle={styles.cancelButtonImage}
+                        source={require("../images/cancel.png")}
+                        onPress={props.hideFilterModal}
+                    />
+                </View>
+            </View>
         </Modal>
     )
 };
@@ -72,15 +87,16 @@ const styles = StyleSheet.create({
         flex: 1, 
         alignItems: "center"
     }, 
-    formRests: {
-        flex: 1,
-        flexDirection: "row",
+    wrapper: {
+        flex: 1
     },
-    submitButton: {
-        bottom: 50
-    }, 
-    cancelButton: {
-        bottom: 10
+    picker: {
+        flex: 1,
+        alignItems: "center", 
+        marginBottom: 10
+    },
+    formRests: {
+        flex: 1
     },
     cancelButtonImage: {
         width: 60,
--- utils/filter.js
+++ utils/filter.js
@@ -1,4 +1,4 @@
 
-export const radioRests = ["American","Asian", "Italian", "Japanese", "Latin", "Mediterranean", "Mexican"];
+export const radioRests = ["American", "Italian", "Asian", "Mexican", "Southern", "French", "Southwestern", "Barbecue", "Indian", "Chinese", "Cajun & Creole", "Mediterranean", "Greek", "English", "Spanish", "Thai", "German", "Moroccan", "Irish", "Japanese", "Cuban", "Hawaiian", "Swedish", "Portuguese", "Russian", "Australian", "Hungarian"];
     
 export const radioPrices = ["$", "$$", "$$$", "$$$$"];
